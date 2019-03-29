46c8427d872c797a4d3489abedcb47c0e87fcef6
Reznikovkg <reznikov.kosta@yandex.ru>
2019-03-29 14:42:53 +0300

    add switch lang

--- app/Resources/js/frontend/header.js
+++ app/Resources/js/frontend/header.js
@@ -7,20 +7,5 @@ $(document).ready(function() {
             $('#mainNav').removeClass("nav-fix");
         }
     });
-
-    $(function(){
-        $('.selectpicker').selectpicker();
-    });
-
-    // $("#switchLang").on('scroll', function () {
-    //     // var display = $('#switchLangList');
-    //
-    //     console.log(1);
-    //     // if (window.pageYOffset > 100) {
-    //     //     $('#mainNav').addClass("nav-fix");
-    //     // } else {
-    //     //     $('#mainNav').removeClass("nav-fix");
-    //     // }
-    // });
 });
 
--- web/js/frontend.js
+++ web/js/frontend.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$(window).on("scroll",function(){window.pageYOffset>100?$("#mainNav").addClass("nav-fix"):$("#mainNav").removeClass("nav-fix")}),$(function(){$(".selectpicker").selectpicker()})});
+$(document).ready(function(){$(window).on("scroll",function(){window.pageYOffset>100?$("#mainNav").addClass("nav-fix"):$("#mainNav").removeClass("nav-fix")})});
