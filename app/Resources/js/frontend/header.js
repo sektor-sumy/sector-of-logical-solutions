2328ba774ea4184bd2e237838b5ddd5e67cf1508
Reznikovkg <reznikov.kosta@yandex.ru>
2019-03-29 14:16:38 +0300

    lang switcher

--- app/Resources/js/frontend/header.js
+++ app/Resources/js/frontend/header.js
@@ -1,10 +1,6 @@
 
 $(document).ready(function() {
-
     $(window).on('scroll', function () {
-        var scrolled = window.pageYOffset;
-
-
         if (window.pageYOffset > 100) {
             $('#mainNav').addClass("nav-fix");
         } else {
@@ -12,4 +8,19 @@ $(document).ready(function() {
         }
     });
 
+    $(function(){
+        $('.selectpicker').selectpicker();
+    });
+
+    // $("#switchLang").on('scroll', function () {
+    //     // var display = $('#switchLangList');
+    //
+    //     console.log(1);
+    //     // if (window.pageYOffset > 100) {
+    //     //     $('#mainNav').addClass("nav-fix");
+    //     // } else {
+    //     //     $('#mainNav').removeClass("nav-fix");
+    //     // }
+    // });
 });
+
--- web/js/frontend.js
+++ web/js/frontend.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$(window).on("scroll",function(){window.pageYOffset;window.pageYOffset>100?$("#mainNav").addClass("nav-fix"):$("#mainNav").removeClass("nav-fix")})});
+$(document).ready(function(){$(window).on("scroll",function(){window.pageYOffset>100?$("#mainNav").addClass("nav-fix"):$("#mainNav").removeClass("nav-fix")}),$(function(){$(".selectpicker").selectpicker()})});
